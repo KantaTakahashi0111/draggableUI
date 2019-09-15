e5986f69a2ba1c08303644b26f26940f0e025f8f
Kanta <tehayan0111@gmail.com>
2019-09-15 23:27:28 +0900

    20190915 add build

---
+++ build/precache-manifest.1ddcfa690c31478ae61764ae8dc295d7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "355084ad0f5b80db7bd224448e82d91f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d6e5ddd93c8e1274f5d1",
+    "url": "/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "8cddcdccefb3807db5fe",
+    "url": "/static/js/2.7035cb4d.chunk.js"
+  },
+  {
+    "revision": "d6e5ddd93c8e1274f5d1",
+    "url": "/static/js/main.e4e14477.chunk.js"
+  },
+  {
+    "revision": "e97ef3bef83ca552f161",
+    "url": "/static/js/runtime~main.a55a2ba9.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
